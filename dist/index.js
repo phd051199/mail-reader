647abd029ec4910f20ad7b2d8f753287eb991136
Duy Pham <phd051199@gmail.com>
2025-05-23 19:52:17 +0700

    fix

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.getAccessToken = getAccessToken;
 const node_server_1 = require("@hono/node-server");
 const hono_1 = require("hono");
 const imapflow_1 = require("imapflow");
 const mailparser_1 = require("mailparser");
 const app = new hono_1.Hono();
-app.get("/email", async (c) => {
+app.get("/", (c) => c.text("ok"));
+app.post("/email", async (c) => {
+    const { raw } = await c.req.json();
+    const [email, pass, refreshToken, clientId] = raw.split("|");
+    const accessToken = await getAccessToken(refreshToken, clientId);
     const client = new imapflow_1.ImapFlow({
         host: "outlook.office365.com",
         port: 993,
         secure: true,
         auth: {
-            user: "RollandeNoonanjanuary832@outlook.com",
-            pass: "6DxjdaIsy388",
-            accessToken: "EwA4BOl3BAAUcDnR9grBJokeAHaUV8R3+rVHX+IAAUE3SXZcs3PKXdT56ldibgyIF1RqXfKS9vMuhoqYljO4+EPXh1Fqtk05QiREwxFWeBq9g69V2Yoch15XsVc7bsmARvIBvAUuD91NiCjcEyhHu4lJWFFz/k4TUv+eT/HbxPvtTFWlBTEC8vt3rgVLY6xp0gr2rOz6rKG4DXohyO2YAYTSEzpj0Hk7Huo9h59cu0I4SiKTUUyZFYY14XYfx6L3IQqleWgfJKmN/Pj6bQAfJe2vsOHVdAtTIucJuTB00+SlhaWr7pgo0n4KbcoL1JxS+khBP64WZf6SQdCF+TJUinZnBytjYITZMp7zR5MDI4fnmaWS/Hg3KI34oY4MM5kQZgAAEAdxXb+mgipyilz4PlrfDjEAA68aNasjWHeVlNOaAXtGJioDqjm0Aqw58MZnuuFVQOe4wYZf5Ui0FIfp00l3IkQj5zZdo2JCAYz4fY9rCFBMEcaosJvhFo66k8G7HIOu/+F/JcX0xioC2D0WqpC2Be3lE0yXYo6gLkNPrQE+MaFjlDsE60UWo/aPPiplj+aDSZQIaE0NpRKmQYKxToVSeNS4uKrClqBD+pp0xvXd1LUJuVP/kmyzvXc4k11C2YYx2lYb5Hql3T3cpdIyc3ApVV0X1f4YXtNfJhg/5hmVr9NMNGe4Kq8pHDBJv8twePDo8xJIll315yFfFv8018l92ZpW7GcvxmatQ74R2YJI9YGrUQmTskIPWUjsveV6UksQt2Ma/oFZ1v+z6hhthysr15ZP40CN61MmRhg+lgdrDRwgX/D54nskiSFbNH+hRQ1FETSxXadrwBV6PPo40GqsRwQ/1RRW7XTIa1xiwxeNPy5Z//m07m/5/gN5pltLDPQNdM+31lTviVNxbAyc0pYwmX8ry5tIyGafP0NFeMFQSgj8WWiF+lFfJpgAkZv0Y1v5KoD4I28KpCVi4T9j6h7CwQhxVUGazLxongkIHbvU/pQ2kHb9fZrFY0GW89JPi4LPZovpUhlfVehlO5GPFMW1n4wnKGjvqb+wlwzNkOlIos5DNFwFKPUYiIHJdZdbFFjHZqPasNRyUwFYLJw6pkR/FvGGmqa0z9wtYPa+dODaVIO54HigE8v9UdZC+hfe53yLsb6K17HymMn48dZPrC4EO7bXRzdVTN7+JVv4oF4uuq+WqRCpX0rCEEmo4J/qVC0F4TLJDb/SQlWcDHg+e3TqzjlinnU4W+dLwpmq5C41Pmk5o57gdWFHbHcIVUk+48Sm8ZceHpmiB2LC6740W6LgdXDLRqF/XG+2rMcNfeS1OVPXCSnVt4/FGUnKOtzYurvsRi7OEi7+6jHo3wvUN3dpvZmh12Nq1rRuDHeqCmT4b8dxZ3jR64DFzjb0+bZ0hJVOCjdOAcyth3Z1tOI+T5cm/VVNgkED",
+            user: email,
+            pass,
+            accessToken,
         },
         logger: false,
     });
     try {
         await client.connect();
         const lock = await client.getMailboxLock("Junk");
+        // const lock = await client.getMailboxLock("INBOX");
         try {
             const latestMessage = await client.fetchOne(client.mailbox.exists, {
                 source: true,
@@ -28,14 +34,15 @@ app.get("/email", async (c) => {
                 return c.json({ error: "No email found" }, 404);
             }
             const parsed = await (0, mailparser_1.simpleParser)(latestMessage.source);
-            const emailData = {
-                from: parsed.from?.text,
-                date: parsed.date,
-                subject: parsed.subject,
-                text: parsed.text,
-                code: getVerificationCodeFromMaildrop(parsed.subject ?? "")
-            };
-            return c.json(emailData);
+            // const emailData = {
+            //   from: parsed.from?.text,
+            //   date: parsed.date,
+            //   subject: parsed.subject,
+            //   text: parsed.text,
+            //   code: getVerificationCodeFromMaildrop(parsed.subject ?? "") ?? "",
+            // };
+            const code = getVerificationCodeFromMaildrop(parsed.subject ?? "") ?? "";
+            return c.json({ code });
         }
         finally {
             lock.release();
@@ -55,4 +62,24 @@ const getVerificationCodeFromMaildrop = (subject) => {
     }
     return null;
 };
-(0, node_server_1.serve)(app);
+async function getAccessToken(refreshToken, clientId) {
+    const body = new URLSearchParams({
+        refresh_token: refreshToken,
+        client_id: clientId,
+        grant_type: "refresh_token",
+    });
+    const response = await fetch("https://login.microsoftonline.com/common/oauth2/v2.0/token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body,
+    });
+    const { access_token } = await response.json();
+    return access_token;
+}
+(0, node_server_1.serve)({
+    fetch: app.fetch,
+    port: 3333,
+});
+console.log("http://localhost:3333");
--- src/index.ts
+++ src/index.ts
@@ -5,30 +5,35 @@ import { simpleParser } from "mailparser";
 
 const app = new Hono();
 
-app.get("/email", async (c) => {
+app.get("/", (c) => c.text("ok"));
+
+app.post("/email", async (c) => {
+  const { raw } = await c.req.json();
+  const [email, pass, refreshToken, clientId] = raw.split("|");
+  const accessToken = await getAccessToken(refreshToken, clientId);
+
   const client = new ImapFlow({
     host: "outlook.office365.com",
     port: 993,
     secure: true,
     auth: {
-      user: "RollandeNoonanjanuary832@outlook.com",
-      pass: "6DxjdaIsy388",
-      accessToken:
-        "EwA4BOl3BAAUcDnR9grBJokeAHaUV8R3+rVHX+IAAUE3SXZcs3PKXdT56ldibgyIF1RqXfKS9vMuhoqYljO4+EPXh1Fqtk05QiREwxFWeBq9g69V2Yoch15XsVc7bsmARvIBvAUuD91NiCjcEyhHu4lJWFFz/k4TUv+eT/HbxPvtTFWlBTEC8vt3rgVLY6xp0gr2rOz6rKG4DXohyO2YAYTSEzpj0Hk7Huo9h59cu0I4SiKTUUyZFYY14XYfx6L3IQqleWgfJKmN/Pj6bQAfJe2vsOHVdAtTIucJuTB00+SlhaWr7pgo0n4KbcoL1JxS+khBP64WZf6SQdCF+TJUinZnBytjYITZMp7zR5MDI4fnmaWS/Hg3KI34oY4MM5kQZgAAEAdxXb+mgipyilz4PlrfDjEAA68aNasjWHeVlNOaAXtGJioDqjm0Aqw58MZnuuFVQOe4wYZf5Ui0FIfp00l3IkQj5zZdo2JCAYz4fY9rCFBMEcaosJvhFo66k8G7HIOu/+F/JcX0xioC2D0WqpC2Be3lE0yXYo6gLkNPrQE+MaFjlDsE60UWo/aPPiplj+aDSZQIaE0NpRKmQYKxToVSeNS4uKrClqBD+pp0xvXd1LUJuVP/kmyzvXc4k11C2YYx2lYb5Hql3T3cpdIyc3ApVV0X1f4YXtNfJhg/5hmVr9NMNGe4Kq8pHDBJv8twePDo8xJIll315yFfFv8018l92ZpW7GcvxmatQ74R2YJI9YGrUQmTskIPWUjsveV6UksQt2Ma/oFZ1v+z6hhthysr15ZP40CN61MmRhg+lgdrDRwgX/D54nskiSFbNH+hRQ1FETSxXadrwBV6PPo40GqsRwQ/1RRW7XTIa1xiwxeNPy5Z//m07m/5/gN5pltLDPQNdM+31lTviVNxbAyc0pYwmX8ry5tIyGafP0NFeMFQSgj8WWiF+lFfJpgAkZv0Y1v5KoD4I28KpCVi4T9j6h7CwQhxVUGazLxongkIHbvU/pQ2kHb9fZrFY0GW89JPi4LPZovpUhlfVehlO5GPFMW1n4wnKGjvqb+wlwzNkOlIos5DNFwFKPUYiIHJdZdbFFjHZqPasNRyUwFYLJw6pkR/FvGGmqa0z9wtYPa+dODaVIO54HigE8v9UdZC+hfe53yLsb6K17HymMn48dZPrC4EO7bXRzdVTN7+JVv4oF4uuq+WqRCpX0rCEEmo4J/qVC0F4TLJDb/SQlWcDHg+e3TqzjlinnU4W+dLwpmq5C41Pmk5o57gdWFHbHcIVUk+48Sm8ZceHpmiB2LC6740W6LgdXDLRqF/XG+2rMcNfeS1OVPXCSnVt4/FGUnKOtzYurvsRi7OEi7+6jHo3wvUN3dpvZmh12Nq1rRuDHeqCmT4b8dxZ3jR64DFzjb0+bZ0hJVOCjdOAcyth3Z1tOI+T5cm/VVNgkED",
+      user: email,
+      pass,
+      accessToken,
     },
     logger: false,
   });
 
   try {
     await client.connect();
-
     const lock = await client.getMailboxLock("Junk");
+    // const lock = await client.getMailboxLock("INBOX");
     try {
       const latestMessage = await client.fetchOne(
         (client.mailbox as any).exists,
         {
           source: true,
-        }
+        },
       );
 
       if (!latestMessage?.source) {
@@ -37,15 +42,15 @@ app.get("/email", async (c) => {
 
       const parsed = await simpleParser(latestMessage.source);
 
-      const emailData = {
-        from: parsed.from?.text,
-        date: parsed.date,
-        subject: parsed.subject,
-        text: parsed.text,
-        code: getVerificationCodeFromMaildrop(parsed.subject ?? "")
-      };
-
-      return c.json(emailData);
+      // const emailData = {
+      //   from: parsed.from?.text,
+      //   date: parsed.date,
+      //   subject: parsed.subject,
+      //   text: parsed.text,
+      //   code: getVerificationCodeFromMaildrop(parsed.subject ?? "") ?? "",
+      // };
+      const code = getVerificationCodeFromMaildrop(parsed.subject ?? "") ?? "";
+      return c.json({ code });
     } finally {
       lock.release();
     }
@@ -56,12 +61,8 @@ app.get("/email", async (c) => {
   }
 });
 
-const getVerificationCodeFromMaildrop =  (
-  subject: string,
-) => {
-  const subjectMatch = subject.match(
-    /Verification Code:\s*([0-9\-]+)/
-  );
+const getVerificationCodeFromMaildrop = (subject: string) => {
+  const subjectMatch = subject.match(/Verification Code:\s*([0-9\-]+)/);
   if (subjectMatch?.[1]) {
     return subjectMatch[1];
   }
@@ -69,4 +70,34 @@ const getVerificationCodeFromMaildrop =  (
   return null;
 };
 
-serve(app);
+export async function getAccessToken(
+  refreshToken: string,
+  clientId: string,
+): Promise<string> {
+  const body = new URLSearchParams({
+    refresh_token: refreshToken,
+    client_id: clientId,
+    grant_type: "refresh_token",
+  });
+
+  const response = await fetch(
+    "https://login.microsoftonline.com/common/oauth2/v2.0/token",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body,
+    },
+  );
+
+  const { access_token } = await response.json();
+  return access_token;
+}
+
+serve({
+  fetch: app.fetch,
+  port: 3333,
+});
+
+console.log("http://localhost:3333");
